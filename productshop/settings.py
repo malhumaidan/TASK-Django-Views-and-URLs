bd7e65e39c9bb1d164b3ae728bed1b1820ece61d
mohammad <malhumaidaan@gmail.com>
2022-10-16 17:03:49 +0300

    Bonus 1

--- productshop/settings.py
+++ productshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(glzf2&bhlec8ei_vzj$w(gzx&nv*!iqqdg$u=pjahcb^+i$-@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
